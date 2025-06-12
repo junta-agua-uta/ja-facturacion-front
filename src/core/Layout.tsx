b3a108c25636a7041004a3ee74dd89727cf9a95a
Ariel <alexarieltonato@hotmail.com>
2025-06-12 11:44:36 -0500

    Correcion en el layout

--- src/core/Layout.tsx
+++ src/core/Layout.tsx
@@ -11,6 +11,7 @@ const Layout = () => {
   const navigate = useNavigate();
   const [userName, setUserName] = useState('Usuario');
   const [userRole, setUserRole] = useState('');
+  const [menuOpen, setMenuOpen] = useState(true);
 
 
   useEffect(() => {
@@ -42,9 +43,13 @@ const Layout = () => {
     return location.pathname.includes(path);
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="flex h-screen bg-gray-100">
-      <aside className="w-64 flex flex-col bg-gray-200 border-r border-gray-300 shadow-xl pb-5">
+      <aside className={`${menuOpen ? 'w-64' : 'w-0'} flex flex-col bg-gray-200 border-r border-gray-300 shadow-xl pb-5 transition-all duration-300 overflow-hidden`}>
         <div className="p-4 flex justify-center">
           <img src="/logo_agua.svg" alt="Logo Agua Pública" className="h-36" />
         </div>
@@ -123,11 +128,43 @@ const Layout = () => {
         </button>
       </aside>
 
-      <main className="flex-1 overflow-auto md:max-w-5xl lg:max-w-7xl mx-auto mt-10 p-6 space-y-6 max-h-full">
+      <main className="flex-1 h-screen overflow-auto md:max-w-5xl lg:max-w-7xl mx-auto  p-6 space-y-6 max-h-full relative">
+        <button
+          onClick={toggleMenu}
+          className="absolute -top-1 left-0 m-4 p-2 bg-gray-200 text-gray-800 rounded-full shadow-lg hover:bg-gray-300 transition-colors duration-200 z-10"
+          aria-label={menuOpen ? "Cerrar menú" : "Abrir menú"}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {menuOpen ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            )}
+          </svg>
+        </button>
+        <div className='mt-10'>
         <Outlet />
+
+        </div>
       </main>
     </div>
   )
 }
 
-export default Layout 
+export default Layout
